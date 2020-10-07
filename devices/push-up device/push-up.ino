a167256cf3c155cd32fd42b37bd031d0b25e86bb
Station Work <friendnt@naver.com>
2020-10-07 13:54:17 +0000

    ultrasonic init

--- devices/push-up device/push-up.ino
+++ devices/push-up device/push-up.ino
@@ -1,1 +1,11 @@
 // push-up device
+# include <Ultrasonic.h>
+Ultrasonic ultrasonic(9,8); // (Trig PIN,Echo PIN)
+void setup() {
+    Serial.begin(9600);
+}
+void loop()
+{
+    Serial.print(ultrasonic.Ranging(CM));
+    delay(500);
+}
