e6fcdcc7988e8f4b3c3fd72f211e281af970bfa6
Station Work <friendnt@naver.com>
2020-10-17 13:10:58 +0000

    add buzzer sound

--- devices/push-up device/push-up.ino
+++ devices/push-up device/push-up.ino
@@ -5,7 +5,9 @@ Ultrasonic ultrasonic(9,8); // (Trig PIN,Echo PIN)
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int btnPin 13;
-
+int speakerPin = 12; //buzzer
+int numTones = 7;
+int tones = [392,392,440,440,392,392,329]; //GGAAGGE
 void setup() {
   	// set up the LCD's number of columns and rows
   	lcd.begin(16, 2);
@@ -24,9 +26,8 @@ void loop()
     lcd.clear();
     
     // when btnPin sign low
-  	if (digitalRead(btnPin) == LOW)
-    {
-      // 
+  	if (digitalRead(btnPin) == LOW){
+      // when serial port available
       while(Serial.available() > 0){
       	lcd.print(ultrasonic.Ranging(CM));
         delay(500);
@@ -35,11 +36,17 @@ void loop()
         lcd.setCursor(0, 1);
         sec = millis() / 1000;
         lcd.print(sec);
-        if(sec == 120)
-        {
+        if(sec == 120){
         	break;
         }
       }
+      // finish sound
+      for(int i = 0; i < numTones; i++){
+        tone(speakerPin, tones[i]);
+        delay(500);
+      }
+      noTone(speakerPin);
+      delay(1000);
     }
   }
 }
